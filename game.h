1e32df76927cf70b74eabb3c45fae5242e4a71c3
Viet281101 <nva281101@gmail.com>
2023-11-29 22:37:42 +0100

    update board

--- game.h
+++ game.h
@@ -4,11 +4,14 @@
 #include <GL/glut.h>
 #include <GL/freeglut.h>  
 
+#include <math.h>
+
 /* pour Mac */
 // #include <GLUT/glut.h>
 
 #define BOARD_SIZE 8
 #define CELL_SIZE 80
+#define DEG2RAD (3.14159/180.0)
 
 int isValidMove(int board[BOARD_SIZE][BOARD_SIZE], int x, int y, int player);
 void makeMove(int board[BOARD_SIZE][BOARD_SIZE], int x, int y, int player);
--- main.c
+++ main.c
@@ -8,21 +8,26 @@ int currentPlayer = 1; //// 1 = white, 2 = black
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    // Draw the Othello board with a single color (#009067)
+    glColor3f(0.0, 0.56, 0.4); // Dark green using the hex color #009067
     for (int x = 0; x < BOARD_SIZE; x++) {
         for (int y = 0; y < BOARD_SIZE; y++) {
-            //// Choose colors for board
-            if ((x + y) % 2 == 0) {
-                glColor3f(1.0, 1.0, 1.0); //// White
-            } else {
-                glColor3f(0.0, 0.0, 0.0); //// Black
-            }
-
-            //// Draw the chessboard squares
             glRecti(x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE);
         }
     }
 
-    //// draw othello chess pieces
+    // Draw the grid lines
+    glColor3f(0.0, 0.0, 0.0); // Black color for the lines
+    glBegin(GL_LINES);
+    for (int i = 0; i <= BOARD_SIZE; i++) {
+        // Vertical lines
+        glVertex2f(i * CELL_SIZE, 0);
+        glVertex2f(i * CELL_SIZE, BOARD_SIZE * CELL_SIZE);
+        // Horizontal lines
+        glVertex2f(0, i * CELL_SIZE);
+        glVertex2f(BOARD_SIZE * CELL_SIZE, i * CELL_SIZE);
+    }
+    glEnd();
 
     glFlush();
 };
